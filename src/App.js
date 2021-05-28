d0dfaf7279ef70a04d57b3860d795f349bc9e7c0
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-05-28 17:07:07 +0100

    added anim

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const setToComplete = () => {
 
   return (
     <>
-    <div className="App h-screen min-h-screen max-h-screen w-screen bg-gradient-to-b from-[#252b2f] to-home-light md:bg-gradient-to-r md:from-[#080D0F] md:via-home-dark md:to-home-light overflow-x-hidden" style={{filter: showModal ? 'blur(16px)' : ""}}>
+    <div className="App h-screen min-h-screen max-h-screen w-screen bg-gradient-to-b from-[#252b2f] to-home-light md:bg-gradient-to-r md:from-[#080D0F] md:via-home-dark md:to-home-light overflow-x-hidden" style={{filter: showModal ? 'blur(10px)' : ""}}>
        <div className="max-w-screen-xl h-full mx-auto flex items-start md:items-center mt-40 md:mt-0 px-0 sm:px-6">
          {complete &&
          <motion.div className="max-w-4xl mx-auto md:mx-0"
@@ -112,7 +112,7 @@ const setToComplete = () => {
        variants={variants}
        animate={remove ? 'isComplete': 'notComplete'}
        >
-         <img src={logo} className={`${complete && 'App-logo'}`} alt="logo" />
+         <img src={logo} className={`${complete ? showModal ? '':'App-logo' : ''}`} alt="logo" />
          <div className="absolute inset-0 h-full w-full flex justify-center items-center">
             <motion.img src={nigeria} alt="nigeria" className="w-12 md:w-auto"
               initial={{ opacity: 0 }}
@@ -126,7 +126,7 @@ const setToComplete = () => {
        variants={variants}
        animate={remove ? 'isCompleteMobile': 'notCompleteMobile'}
        >
-         <img src={logo} className={`${complete && 'App-logo'}`} alt="logo" />
+         <img src={logo} className={`${complete ? showModal ? '':'App-logo' : ''}`} alt="logo" />
          <div className="absolute inset-0 h-full w-full flex justify-center items-center">
             <motion.img src={nigeria} alt="nigeria" className="w-12 md:w-auto"
               initial={{ opacity: 0 }}
@@ -136,8 +136,14 @@ const setToComplete = () => {
          </div>
        </motion.div>
     </div>
-    {showModal &&
-      <div className="fixed inset-0 w-screen max-h-screen min-h-screen bg-[rgba(0,0,0,0.55)] flex justify-center items-center px-4 md:px-8">
+    <AnimatePresence> {
+        showModal && (
+      <motion.div className="fixed inset-0 w-screen max-h-screen min-h-screen bg-[rgba(0,0,0,0.55)] flex justify-center items-center px-4 md:px-8"
+        initial={{ opacity: 0 }}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}
+        transition={{ duration: 0.3}}
+      >
          <div className="max-w-md py-[64px] px-4 md:px-10 font-bold rounded-2xl modal-color relative">
           <button onClick={toggleModal} className="absolute top-4 left-4 md:left-8">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -149,8 +155,9 @@ const setToComplete = () => {
            <input type="email" className="w-full border border-white bg-transparent rounded py-4 mt-12 text-base font-semibold px-4 placeholder-[rgba(255,255,255,0.18)]" placeholder="Email Address" />
             <button className="bg-[#7FD0FA] text-white  rounded-[5px] w-full h-auto px-6 py-4 font-semibold text-sm mt-12">Sign up</button>
          </div>
-       </div>
-       }
+       </motion.div>
+        )}
+       </ AnimatePresence>
     </>
   );
 }
